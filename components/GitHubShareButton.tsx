a863ff9affe6133602f577c4ce48cd2f1a4e3bc4
Zach Chan <564737735@qq.com>
2021-11-16 17:03:16 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -4,33 +4,42 @@ import styles from './styles.module.css'
 
 export const GitHubShareButton: React.FC = () => {
   return (
-    <div>
-  <p id='cl'
-     class='styles_githubCorner__1WYyN'
+    <p id='cl'
+     className={styles.githubCorner}
      aria-label='View source on GitHub'>
-    <svg width='30'
-         height='30'
-         viewBox='0 0 250 250'
-         style='fill:#1ed760;color:#fff;position:absolute;z-index:1001;top:0;right:0;border:0;transform:scale(1, 1)'
-         aria-hidden='true'>
-      <svg xmlns='http://www.w3.org/2000/svg'
-           viewBox='0 0 1333.33 1333.3'
+    <svg viewBox='0 0 1333.33 1333.3'
            shape-rendering='geometricPrecision'
            text-rendering='geometricPrecision'
            image-rendering='optimizeQuality'
            fill-rule='evenodd'
-           clip-rule='evenodd'>
+           clip-rule='evenodd'
+         width='30'
+         height='30'
+         viewBox='0 0 250 250'
+         style={{
+          fill:'#1ed760',
+          color: '#fff',
+          position: 'absolute',
+          zIndex: 1001,
+          top: 0,
+          right: 0,
+          border: 0,
+          transform: 'scale(1, 1)'
+          }}
+          aria-hidden='true'
+          >
           <path d='M666.66 0C298.48 0 0 298.47 0 666.65c0 368.19 298.48 666.65 666.66 666.65 368.22 0 666.67-298.45 666.67-666.65C1333.33 298.49 1034.88.03 666.65.03l.01-.04zm305.73 961.51c-11.94 19.58-37.57 25.8-57.16 13.77-156.52-95.61-353.57-117.26-585.63-64.24-22.36 5.09-44.65-8.92-49.75-31.29-5.12-22.37 8.84-44.66 31.26-49.75 253.95-58.02 471.78-33.04 647.51 74.35 19.59 12.02 25.8 37.57 13.77 57.16zm81.6-181.52c-15.05 24.45-47.05 32.17-71.49 17.13-179.2-110.15-452.35-142.05-664.31-77.7-27.49 8.3-56.52-7.19-64.86-34.63-8.28-27.49 7.22-56.46 34.66-64.82 242.11-73.46 543.1-37.88 748.89 88.58 24.44 15.05 32.16 47.05 17.12 71.46V780zm7.01-189.02c-214.87-127.62-569.36-139.35-774.5-77.09-32.94 9.99-67.78-8.6-77.76-41.55-9.98-32.96 8.6-67.77 41.56-77.78 235.49-71.49 626.96-57.68 874.34 89.18 29.69 17.59 39.41 55.85 21.81 85.44-17.52 29.63-55.89 39.4-85.42 21.8h-.03z'
                   fill='#1ed760'
           ></path>
-      </svg>
     </svg>
-  </p>
-  <p id='list'
-     style='display: block;
-     position: absolute;
-     top: auto;
-     right: 0px;'>
+    <p id='list'
+       style={{
+       display: 'block',
+    position: 'absolute',
+    top: 'auto',
+    right: '0px'
+    }}
+    >
     <iframe src='https://open.spotify.com/embed/playlist/1B2BamhH9YYUoYvJaGd4lF'
             width='300'
             height='380'
@@ -38,8 +47,7 @@ export const GitHubShareButton: React.FC = () => {
             allowtransparency='true'
             allow='encrypted-media'>
     </iframe>
-  </p>
-  
-</div>
+    </p>
+  </p
   )
 }
